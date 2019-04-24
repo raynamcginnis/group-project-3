54ce97a2d82ea3df8375555dd0f9f2600ce673c6
TGHeadle1371 <tgheadle@gmail.com>
2019-04-23 20:18:47 -0600

    Nav component and Search Component

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const path = require("path");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
@@ -10,15 +10,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+    app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
+    { useNewUrlParser: true }
+);
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+});
 // Start the API server
 app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
